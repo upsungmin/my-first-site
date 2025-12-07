d0097fcd1f1cc662d09c68ff13e0c0eeefd2e5f7
unknown <gmrgud03040@naver.com>
2025-12-07 14:53:20 +0900

    Fix server/client boundary issue in metadata

--- lib/metadata.ts
+++ lib/metadata.ts
@@ -2,13 +2,13 @@ import { defaultConfig } from "@/components/header"
 
 // 메타데이터 헬퍼 함수
 export function getMetadata() {
-  // localStorage는 서버 사이드에서 사용할 수 없으므로 기본값 사용
+  // 기본값만 사용 (localStorage는 서버에서 불가능)
   const defaultInfo = {
     name: "당신의 이름",
     title: "프론트엔드 개발자",
-    description: defaultConfig.siteDescription, // header의 defaultConfig에서 가져오기
+    description: "창의적인 아이디어로 웹 경험을 디자인합니다.", // 직접 문자열로 변경
     profileImage: "",
-    siteTitle: defaultConfig.siteTitle, // header의 defaultConfig에서 가져오기
+    siteTitle: "나의 포트폴리오", // 직접 문자열로 변경
   }
   
   // 클라이언트 사이드에서만 localStorage 접근
